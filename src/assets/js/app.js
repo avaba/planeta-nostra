54a3bb4c80b2d2152aef5191abbacbd60af0f625
Иголышков Сергей <sigolyshkov@yandex.ru>
2021-12-15 13:38:36 +0300

    traning

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -3,10 +3,31 @@ const PreloaderAnswer = document.querySelector('.preloader__answer');
 const Art = document.querySelectorAll('.art.scene');
 const Puzzle = document.querySelectorAll('.puzzle');
 const Finish = document.querySelectorAll('.finish');
-const Video = document.querySelector('.finish__video')
+const Video = document.querySelector('.video-hover');
+const GetLink = document.querySelectorAll('.finish-footer__item-link');
+const Training = document.querySelector('.training');
+const VideoTraining = document.querySelector('.training__video video');
+const GetStarted = document.querySelector('.get-started')
+const GetStartedSection = document.querySelectorAll('.get-started__section')
 let Count = 0;
+let GetStartedActive = 0;
 
-console.log(Art.length);
+GetLink.forEach((item, index) => {
+  item.addEventListener('click', () => {
+    const data = item.getAttribute('data-section')
+    console.log(data)
+    Finish[0].classList.add('scroll-down');
+    GetStarted.classList.add('scroll-down')
+  });
+});
+
+Training.addEventListener('click', () => {
+  Training.classList.add('start');
+  setTimeout(() => {
+    VideoTraining.classList.add('show');
+    VideoTraining.play();
+  }, 7500);
+});
 
 const PreloaderTick = setInterval(() => {
   setTimeout(() => {
@@ -69,4 +90,23 @@ setTimeout(() => {
     }
   }, 600);
 }, 2000);
+
+GetLink.forEach(item => {
+  item.addEventListener('mouseover', () => {
+    Video.classList.add('play');
+    Video.play();
+    Video.addEventListener('ended', function () {
+      Video.currentTime = 3;
+    });
+    Video.addEventListener('timeupdate', function () {
+      if (Video.currentTime >= 8.2) {
+        Video.currentTime = 3;
+      }
+    });
+  });
+  item.addEventListener('mouseout', () => {
+    Video.classList.remove('play');
+    Video.pause();
+  });
+});
 
